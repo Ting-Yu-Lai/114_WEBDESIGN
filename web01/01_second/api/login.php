827fb1fabbe5b8e4814d64f5a5bd9e15b3d3c417
Akuma_Yu <manjusaka1110@gmail.com>
2025-07-04 09:47:35 +0800

    完成紀錄seesion

--- web01/01_second/api/login.php
+++ web01/01_second/api/login.php
@@ -3,6 +3,7 @@
     $user=$Admin->count($_POST);
 
     if($user){
+        $_SESSION['login']=1;
         to("../backend.php");
     }else {
         echo "<script>alert('帳號或密碼錯誤');location.replace('../index.php?do=login')</script>";
