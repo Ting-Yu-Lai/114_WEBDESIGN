cd4c606343c35e5b507f63f82f04b7f6bd8ccf66
Akuma_Yu <manjusaka1110@gmail.com>
2025-07-10 20:04:10 +0800

    練習DB function  含資料庫建立 25min

--- web01/01_four/api/db.php
+++ web01/01_four/api/db.php
@@ -2,23 +2,23 @@
 session_start();
 date_default_timezone_set("Asia/Taipei");
 
-function to ($url) {
-    "Location: $url";
-}
-
 function dd($data)
 {
     echo "<pre>";
     print_r($data);
-    echo "<pre>";
+    echo "</pre>";
+}
+
+function to($url)
+{
+    header("Location:$url");
 }
 
 class DB
 {
-
-    private $table;
-    private $pdo;
     private $dsn = "mysql:host=localhost;dbname=db04;charset=utf8";
+    private $pdo;
+    private $table;
 
     function __construct($table)
     {
@@ -39,6 +39,7 @@ class DB
     {
         return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
+
     function e($sql)
     {
         return $this->pdo->exec($sql);
@@ -61,6 +62,7 @@ class DB
         }
         return $this->q($sql);
     }
+
     function count(...$arg)
     {
         if (isset($arg[0])) {
@@ -81,40 +83,38 @@ class DB
 
     function find($id)
     {
-            $sql = "SELECT * FROM $this->table ";
-            if (is_array($id)) {
-                $tmp = $this->a2s($id);
-                $sql .= " WHERE " . join(" AND ", $tmp);
-            } else {
-                $sql .= " WHERE `id` = '$id'";
-            }
-
-        return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
-    }
-    function del($id)
-    {
-            $sql = "DELETE FROM $this->table ";
-            if (is_array($id)) {
-                $tmp = $this->a2s($id);
-                $sql .= " WHERE " . join(" AND ", $tmp);
-            } else {
-                $sql .= " WHERE `id` = '$id'";
-            }
-
+        $sql = "SELECT * FROM $this->table ";
+        if (is_array($id)) {
+            $tmp = $this->a2s($id);
+            $sql .= " WHERE " . join(" AND ", $tmp);
+        } else {
+            $sql .= " WHERE `id` = '$id'";
+        }
         return $this->e($sql);
     }
 
+    function del($id)
+    {
+        $sql = "DELETE FROM $this->table ";
+        if (is_array($id)) {
+            $tmp = $this->a2s($id);
+            $sql .= " WHERE " . join(" AND ", $tmp);
+        } else {
+            $sql .= " WHERE `id` = '$id'";
+        }
+        return $this->e($sql);
+    }
 
-    function sav($array) {
+    function save($array) {
         if(isset($array['id'])) {
-            $sql = "UPDATE $this->table SET";
-            $tmp = $this->a2s($sql);
-            $sql .= join(" , ",$tmp) . " WHERE `id`='{$array['id']}'";
-        }else {
-            $sql = "INSERT INTO $this->table";
+            $sql = "UPDATE $this->table SET ";
+            $tmp = $this->a2s($array);
+            $sql .= join(" , ",$tmp) . " WHERE `id` = '{$array['id']}'";
+        } else {
+            $sql = "INSERT INTO $this->table ";
             $keys = "`" . join("`,`",array_keys($array)) . "`";
             $values = "'" . join("','",array_values($array)) . "'";
-            $sql .= "($keys) VALUES ($values) ";
+            $sql .= "($keys) VALUES ($values)";
         }
         return $this->e($sql);
     }
@@ -123,11 +123,19 @@ class DB
 }
 
 $Title = new DB('title');
+$Total = new DB('total');
 $Ad = new DB('ad');
 $Admin = new DB('admin');
-$Menu = new DB('menu');
-$Image = new DB('image');
-$Mvim = new DB('mvim');
-$Total = new DB('total');
-$Bottom = new DB('bottom');
 $News = new DB('news');
+$Bottom = new DB('bottom');
+$Image = new DB('image');
+$Menu = new DB('menu');
+$Mvim = new DB('mvim');
+
+if(!isset($_SESSION['view'])) {
+    $t = $Total->find(1);
+    $t['total']++;
+    $Total->save($t);
+    $_SESSION['view']=1;
+}
+
