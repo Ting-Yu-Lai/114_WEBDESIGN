4ffbacc97fc4f62004439174433d0d592fa79288
Akuma_Yu <manjusaka1110@gmail.com>
2025-07-12 16:55:22 +0800

    news 網頁完成

--- web01/01_four/front/news.php
+++ web01/01_four/front/news.php
@@ -10,8 +10,60 @@
     </marquee>
     <div style="height:32px; display:block;"></div>
     <!--正中央-->
+    <h3>更多消息顯示區</h3>
+    <hr>
+    <?php
+    $all = $News->count(['sh' => 1]);
+    $div = 5;
+    $page = ceil($all / $div);
+    $now = $_GET['p'] ?? 1;
+    $start = ($now - 1) * $div;
+    $news = $News->all(['sh' => 1], " limit $start,$div");
+    ?>
+    <ol start="<?= $start + 1; ?>">
+        <?php
+        foreach ($news as $n) {
+            echo  "<li class='sswww'>";
+            echo mb_substr($n['text'],0,25);
+            echo "<span class='all' style='display:none'>";
+            echo $n['text'];
+            echo "</span>";
+            echo  "</li>";
+        }
+        ?>
+    </ol>
     <div style="text-align:center;">
-        <a class="bl" style="font-size:30px;" href="?do=meg&p=0">&lt;&nbsp;</a>
-        <a class="bl" style="font-size:30px;" href="?do=meg&p=0">&nbsp;&gt;</a>
+        <?php
+        if($now - 1 > 0):?>
+        <a class="bl" style="font-size:30px;" href="?do=news&p=<?=$now-1;?>"> < </a>
+        <?php endif;?>
+
+        <?php for($i=1;$i<=$page;$i++):
+            $size = ($now==$i)?'24px':'';?>
+        <a href="?do=news&p=<?=$i;?>" style="font-size: <?=$size;?>;"><?=$i;?></a>
+        <?php endfor;?>
+        
+        <?php if($now + 1 <= $page):?>
+        <a class="bl" style="font-size:30px;" href="?do=news&p=<?=$now+1;?>"> > </a>
+        <?php endif;?>
     </div>
 </div>
+
+<div id="alt"
+    style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">
+</div>
+<script>
+    $(".sswww").hover(
+        function() {
+            $("#alt").html("" + $(this).children(".all").html() + "").css({
+                "top": $(this).offset().top - 50
+            })
+            $("#alt").show()
+        }
+    )
+    $(".sswww").mouseout(
+        function() {
+            $("#alt").hide()
+        }
+    )
+</script>
