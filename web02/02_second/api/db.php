5e4d2de12aeafa7eb4bbbb3705d7f8f399d1cd0d
Akuma_Yu <manjusaka1110@gmail.com>
2025-07-14 21:37:29 +0800

    完成素材整理，index.php一些功能，先把管理者帳密匯入db

---
+++ web02/02_second/api/db.php
@@ -0,0 +1,135 @@
+<?php
+session_start();
+date_default_timezone_set("Asia/Taipei");
+
+
+function dd($data)
+{
+    echo "<pre>";
+    print_r($data);
+    echo "</pre>";
+}
+
+function to($url)
+{
+    header("Location:$url");
+}
+
+
+class DB
+{
+    private  $dsn = "mysql:host=localhost;dbname=db01;charset=utf8;";
+    private  $pdo;
+    private  $table;
+
+    function __construct($table)
+    {
+        $this->table = $table;
+        $this->pdo = new PDO($this->dsn, 'root', '');
+    }
+
+    function a2s($array)
+    {
+        $tmp = [];
+        foreach ($array as $key => $value) {
+            $tmp[] = "`$key` = '$value'";
+        }
+        return $tmp;
+    }
+
+    function all(...$arg)
+    {
+        $sql = "SELECT * FROM $this->table ";
+        if (isset($arg[0])) {
+            if (is_array($arg[0])) {
+                $tmp = $this->a2s($arg[0]);
+                $sql .= " WHERE " . join(" AND ", $tmp);
+            } else {
+                $sql .= $arg[0];
+            }
+        }
+        if (isset($arg[1])) {
+            $sql .= $arg[1];
+        }
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    function count(...$arg)
+    {
+        $sql = "SELECT COUNT(*) FROM $this->table ";
+        if (isset($arg[0])) {
+            if (is_array($arg[0])) {
+                $tmp = $this->a2s($arg[0]);
+                $sql .= " WHERE " . join(" AND ", $tmp);
+            } else {
+                $sql .= $arg[0];
+            }
+        }
+        if (isset($arg[1])) {
+            $sql .= $arg[1];
+        }
+        return $this->pdo->query($sql)->fetchColumn();
+    }
+
+    function sum($col, ...$arg)
+    {
+        $sql = "SELECT sum($col) FROM $this->table ";
+        if (isset($arg[0])) {
+            if (is_array($arg[0])) {
+                $tmp = $this->a2s($arg[0]);
+                $sql .= " WHERE " . join(" AND ", $tmp);
+            } else {
+                $sql .= $arg[0];
+            }
+        }
+        if (isset($arg[1])) {
+            $sql .= $arg[1];
+        }
+        return $this->pdo->query($sql)->fetchColumn();
+    }
+
+    function find($id)
+    {
+        $sql = "SELECT * FROM $this->table ";
+        if (is_array($id)) {
+            $tmp = $this->a2s($id);
+            $sql .= " WHERE " . join(" AND ", $tmp);
+        } else {
+            $sql .= " WHERE `id` = '$id'";
+        }
+        return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+    }
+
+    function del($id)
+    {
+        $sql = "DELETE FROM $this->table ";
+        if (is_array($id)) {
+            $tmp = $this->a2s($id);
+            $sql .= " WHERE " . join(" AND ", $tmp);
+        } else {
+            $sql .= " WHERE `id` = '$id'";
+        }
+        return $this->pdo->exec($sql);
+    }
+
+    function save($array)
+    {
+        if (isset($array['id'])) {
+            $sql = "UPDATE $this->table SET ";
+            $tmp = $this->a2s($array);
+            $sql .= join(",", $tmp) . " WHERE `id` = '{$array['id']}'";
+        } else {
+            $sql = "INSERT INTO $this->table ";
+            $keys = "`" . join("`,`", array_keys($array)) . "`";
+            $values = "'" . join("','", array_values($array)) . "'";
+            $sql .= "($keys) VALUES ($values)";
+        }
+        return $this->pdo->exec($sql);
+    }
+}
+
+$User = new DB('user');
+$User->save(['acc' => 'admin', 'pw' => '1234', 'email' => 'admin@labor.gov.tw']);
+$User->save(['acc' => 'test', 'pw' => '5678', 'email' => 'test@labor.gov.tw']);
+$User->save(['acc' => 'mem01', 'pw' => 'mem01', 'email' => 'mem01@labor.gov.tw']);
+$User->save(['acc' => 'mem02', 'pw' => 'mem02', 'email' => 'mem02@labor.gov.tw']);
---
+++ web02/02_second/front/login.php
@@ -0,0 +1,24 @@
+<fieldset>
+    <legend>會員登入</legend>
+    <div class="ct">
+        <div>
+            <label for="acc">帳號</label>
+            <input type="text" name="acc">
+        </div>
+        <div>
+            <label for="pw">密碼</label>
+            <input type="password" name="pw">
+        </div>
+        <div>
+            <div style="display: inline-block;">
+                <input type="button" value="登入">
+                <input type="reset" value="清除">
+            </div>
+            <div style="display: inline-block;">
+                <a href="forgot.php">忘記密碼</a>
+                |
+                <a href="reg.php">尚未註冊</a>
+            </div>
+        </div>
+    </div>
+</fieldset>
