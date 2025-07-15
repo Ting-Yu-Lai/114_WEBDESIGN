de3e1341d0597260562af27b25677be8faeebb4b
Akuma_Yu <manjusaka1110@gmail.com>
2025-07-15 14:20:38 +0800

    完成最新文章 人氣文章 分類網址的功能

---
+++ web02/02_first/front/news.php
@@ -0,0 +1,54 @@
+<div class="nav" style="margin-bottom:20px"> 
+    <!-- 我要根據點選的分類網址更新文字內容，所以我要賦予他一個id 或 class -->
+    目前位置:首頁 > 最新文章區
+</div>
+
+<table style="width:95%;margin:auto;">
+    <tr class="ct">
+        <td style="width:20%;">標題</td>
+        <td style="width:60%;">內容</td>
+        <td></td>
+    </tr>
+        <?php
+    $total = $News->count();
+    $div = 5;
+    $pages = ceil($total/$div);
+    $now = $_GET['p']??1;
+    $start= ($now - 1)*$div;
+    $rows = $News->all(" limit $start,$div");
+    foreach($rows as $idx => $row):
+    ?>
+    <tr>
+        <td><?=$row['title'];?></td>
+        <td>
+        <div class="short"><?=mb_substr($row['text'], 0, 30);?>...</div>
+        <div class="all"></div>
+        </td>
+        <td>
+            <!-- 按讚功能 -->
+        </td>
+    </tr>
+        <?php
+    endforeach;
+    ?>
+</table>
+<div class="ct">
+<?php
+        if($now-1>0):
+        ?>
+        <a href="?do=news&p=<?=$now-1;?>" style="font-size: 18px;"> < </a>
+        <?php endif;?>
+        <?php
+        for($i=1;$i<=$pages;$i++) {
+            $size = ($i == $now)? '24px':'';
+            ?>
+            <a href="?do=news&p=<?=$i?>"  style="font-size:<?=$size;?>"><?=$i;?></a>
+            <?php
+        }
+?>
+<?php
+if($now+1<=$pages):
+?>
+<a href="?do=news&p=<?=$now+1;?>" style="font-size: 18px;"> > </a>
+<?php endif;?>
+</div>
--- web02/02_first/front/po.php
+++ web02/02_first/front/po.php
@@ -1,9 +1,11 @@
 <div class="nav" style="margin-bottom:20px"> 
+    <!-- 我要根據點選的分類網址更新文字內容，所以我要賦予他一個id 或 class -->
     目前位置:首頁 > 分類網誌 > <span id="NavType">慢性病防治</span>
 </div>
 
 <fieldset style="width:120px;display:inline-block;vertical-align:top;" >
     <legend>分類網址</legend>
+    <!-- 因為要用分類網址去更動上面Nav的文字內容，所以我要給他一個class(type-link)抓取我的text去改變上面的文字 -->
     <div><a class="type-link" data-type='1'>健康新知</a></div>
     <div><a class="type-link" data-type='2'>菸害防治</a></div>
     <div><a class="type-link" data-type='3'>癌症防治</a></div>
@@ -15,11 +17,14 @@
     <div id="Post"></div>
 </fieldset>
 <script>
+    // tpyeId傳進去的值是跟資料庫內的type一樣，所以才會抓取到數值
      getList(1);
 
     $(".type-link").on("click",function() {
+        // 透過抓取typelink的字體去更新上面NavType
         let type = $(this).text();
         $("#NavType").text(type);
+        // 給標題賦予id，從get_type_list拿到資料表的同樣id的title
         let typeId = $(this).data("type");
         getList(typeId);
 
@@ -38,9 +43,9 @@
         getPost(postId);
     })
 
-    // getList 會有時間續的問題，如果把post在前面先執行，就會有先呈現在綁定所以可能會有無法顯示的問題
+    // getList 會有時間緒的問題，如果把post在前面先執行，就會有先呈現在綁定所以可能會有無法顯示的問題
     function getList(type) {
-             $.get("./api/get_type_list.php",{type},function(list){
+            $.get("./api/get_type_list.php",{type},function(list){
             $("#Post").html("");
             $("#TypeList").html(list);
 
