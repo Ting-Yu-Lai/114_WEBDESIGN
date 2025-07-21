4cbd1239a4c01e1830a87f51e870bfaeca2bddeb
Akuma_Yu <manjusaka1110@gmail.com>
2025-07-21 13:07:05 +0800

    註解一下程式碼

--- web02/02_first/front/que.php
+++ web02/02_first/front/que.php
@@ -8,21 +8,25 @@
             <th>結果</th>
             <th>狀態</th>
         </tr>
+        <!-- 拿出subject_id=0的標題 -->
         <?php
         $rows = $Que->all(['subject_id'=>0]);
         foreach ($rows as $key => $row):
         ?>
         <tr class="ct">
+            <!-- key從0開始所以要+1 -->
             <td><?=$key+1;?></td>
             <td><?=$row['text'];?></td>
             <td><?=$row['vote'];?></td>
             <td>
+                <!-- 結果要帶著id過去查看，去配對選項 -->
                 <a href="?do=result&id=<?=$row['id'];?>">結果</a>
             </td>
             <td>
                 <?php
                 if(isset($_SESSION['login'])):
                 ?>
+                <!-- 參與投票要帶著id過去查看，去配對選項，然後記總票數 -->
                 <a href="?do=vote&id=<?=$row['id'];?>">
                     參與投票
                 </a>
--- web02/02_first/front/result.php
+++ web02/02_first/front/result.php
@@ -17,6 +17,8 @@ $options=$Que->all(['subject_id'=>$_GET['id']]);
     <?php
     foreach($options as $key => $option):
         // 拿出標題的票數 = 總票數的意思
+        // 為了避免總票數是 0（會導致除以 0 錯誤），所以使用 ($subject['vote']==0)?1:...。
+        // 如果為 0 就暫時設成 1，這樣 除法 不會報錯。
         $total = ($subject['vote']==0)?1:$subject['vote'];
         $rate = $option['vote']/$total;
     ?>
@@ -26,8 +28,10 @@ $options=$Que->all(['subject_id'=>$_GET['id']]);
             <?=$option['text'];?>
         </span>
         <div style="width: 49%;display:inline-block;">
+        <!-- 為什麼乘以 0.9？這是為了保留右邊資訊欄位空間，因為進度條只佔畫面大約 90% 的寬度，剩下 10% 留給票數與百分比顯示： -->
         <div class="line" style="width:<?=$rate*0.9*100;?>%"></div>
         <div class="info" style="width:20%;">
+
             <?=$option['vote'];?> 票(<?=round($rate*100,2);?>%)
         </div>
         </div>
