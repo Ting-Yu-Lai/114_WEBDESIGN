abe125566bbffc410a52951993b45453c6259036
Akuma_Yu <manjusaka1110@gmail.com>
2025-07-21 10:38:47 +0800

    完成餐與投票功能

---
+++ web02/02_first/api/vote.php
@@ -0,0 +1,14 @@
+<?php
+include_once './db.php';
+
+$option = $Que->find($_POST['option']);
+// 拿到這個option裡面的id的id
+$subject = $Que->find($option['subject_id']);
+$option['vote']++;
+//標題也要+1 代表總票數
+$subjectp['vote']++;
+
+$Que->save($option);
+$Que->save($subject);
+
+to("index.php?do=result&id={$subject['id']}");
---
+++ web02/02_first/front/vote.php
@@ -0,0 +1,24 @@
+<?php
+$subject = $Que->find($_GET['id']);
+$options=$Que->all(['subject_id'=>$_GET['id']]);
+
+?>
+<fieldset>
+    <legend>目前位置：首頁  > 問卷調查  > <?=$subject['text'];?></legend>
+    <h3><?=$subject['text'];?></h3>
+    <form action="./api/vote.php" method="post">
+    <?php
+    foreach($options as $key => $option):
+    ?>
+    <div>
+        <input type="radio" name="option" value="<?=$option['id'];?>">
+        <span>
+            <?=$option['text'];?>
+        </span>
+    </div>
+    <?php endforeach;?>
+    <div class="ct">
+        <input type="submit" value="我要投票">
+    </div>
+    </form>
+</fieldset>
