89aabb77dbae926a66d83f004100a21c59828559
Akuma_Yu <manjusaka1110@gmail.com>
2025-07-22 15:23:31 +0800

    排序功能

--- web03/01_first/back/poster.php
+++ web03/01_first/back/poster.php
@@ -1,4 +1,6 @@
 
+
+<!-- 先列出預告片新單跟新增預告片，先完成新增預告片，在處理預告片清單 -->
 <form action="./api/edit_poster.php" method="post" enctype="multipart/form-data">
     <div style="height: 300px;">
         <h3 class="ct" style="margin: 0;">
@@ -13,14 +15,18 @@
         <div style="overflow: auto;height:200px;" class="ct">
             <?php
             $posters = $Poster->all(' order by `rank`');
-            foreach($posters as $poster):
+            foreach($posters as $idx => $poster):
+                // 是要整個資料表撈出來的陣列-1
+                $prev = ($idx-1 >= 0)? $posters[$idx-1]['id'] : $poster['id'];
+                $next = ($idx + 1 <count($posters))? $posters[$idx+1]['id'] : $poster['id'];
             ?>
             <div style="display: flex; justify-content: space-between; align-items: center; background-color:white; margin-bottom:3px;" class="ct">
                 <div  style="width:24.5%;" ><img src="./image/<?=$poster['img'];?>" style="width: 60px;height:80px;"></div>
                 <div  style="width:24.5%;" ><input type="text" name="name[]" value="<?=$poster['name'];?>"></div>
                 <div  style="width:24.5%;" >
-                    <button type="button">往上</button>
-                    <button type="button">往下</button>
+                    <!-- id不只要有我的還要我的上一筆下一筆 -->
+                    <button type="button" class="swBtn" data-sw='<?=$prev;?>' data-id=<?=$poster['id'];?>>往上</button>
+                    <button type="button" class="swBtn" data-sw='<?=$next;?>' data-id=<?=$poster['id'];?>>往下</button>
                 </div>
                 <div  style="width:24.5%;" >
                     <input type="checkbox" name="sh[]" value="<?=$poster['id'];?>" <?=($poster['sh']==1)?'checked':'';?> >顯示
@@ -62,3 +68,14 @@
     </form>
 </div>
 <!-- 畫面建置完去設置資料表 -->
+ <script>
+    $('.swBtn').on('click',function(){
+        let id = $(this).data('id');
+        let sw = $(this).data('sw');
+
+        $.post('./api/sw.php',{table:'Poster',id,sw},(res)=>{
+            location.reload();
+        })
+    })
+
+ </script>
