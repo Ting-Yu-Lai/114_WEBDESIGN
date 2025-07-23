dc12dfc70789c576ba4145b55920ed87bd10d2b9
Akuma_Yu <manjusaka1110@gmail.com>
2025-07-23 20:55:06 +0800

    完成素材整理，前後端畫面整理，db.php完成，poster資料表建立

---
+++ web03/03_second/api/db.php
@@ -0,0 +1,141 @@
+<?php
+session_start();
+date_default_timezone_set("Asia/Taipei");
+
+function a2s($array) {
+    $tmp = [];
+    foreach($array as $key => $value) {
+        $tmp[]="`$key` = '$value'";
+    }
+    return $tmp;
+}
+
+function to($url) {
+    header("Location:$url");
+}
+
+class DB {
+    private $dsn = "mysql:host=localhost;dbname=web03;charset=utf8;";
+    private $table;
+    private $pdo;
+
+    function __construct($table)
+    {
+        $this->table = $table;
+        $this->pdo = new PDO($this->dsn, 'root', '');
+    }
+
+    function a2s($array) {
+    $tmp = [];
+    foreach($array as $key => $value) {
+        $tmp[]="`$key` = '$value'";
+    }
+    return $tmp;
+    }
+    function q($sql) {
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    function all(...$arg) {
+        $sql = "SELECT * FROM $this->table ";
+        if(isset($arg[0])) {
+            if(is_array($arg[0])) {
+                $tmp = $this->a2s($arg[0]);
+                $sql .= " WHERE " . join(" AND ",$tmp);
+            }else{
+                $sql .= $arg[0];
+          }
+        }
+         if(isset($arg[1])) {
+            $sql .= $arg[1];
+         }
+         return $this->q($sql);
+    }
+    function count(...$arg) {
+        $sql = "SELECT COUNT(*) FROM $this->table ";
+        if(isset($arg[0])) {
+            if(is_array($arg[0])) {
+                $tmp = $this->a2s($arg[0]);
+                $sql .= " WHERE " . join(" AND ",$tmp);
+            }else{
+                $sql .= $arg[0];
+          }
+        }
+         if(isset($arg[1])) {
+            $sql .= $arg[1];
+         }
+         return $this->pdo->query($sql)->fetchColumn();
+    }
+
+    function sum($col,...$arg) {
+        $sql = "SELECT SUM($col) FROM $this->table ";
+        if(isset($arg[0])) {
+            if(is_array($arg[0])) {
+                $tmp = $this->a2s($arg[0]);
+                $sql .= " WHERE " . join(" AND ",$tmp);
+            }else{
+                $sql .= $arg[0];
+          }
+        }
+         if(isset($arg[1])) {
+            $sql .= $arg[1];
+         }
+         return $this->q($sql);
+    }
+
+    function max($col,...$arg) {
+        $sql = "SELECT MAX($col) FROM $this->table ";
+        if(isset($arg[0])) {
+            if(is_array($arg[0])) {
+                $tmp = $this->a2s($arg[0]);
+                $sql .= " WHERE " . join(" AND ",$tmp);
+            }else{
+                $sql .= $arg[0];
+          }
+        }
+         if(isset($arg[1])) {
+            $sql .= $arg[1];
+         }
+         return $this->q($sql);
+    }
+
+    function find($id) {
+        $sql = "SELECT FROM $this->table ";
+        if(is_array($id)) {
+                $tmp = $this->a2s($id);
+                $sql .= " WHERE " . join(" AND ",$tmp);
+            }else{
+                $sql .= " WHERE `id` = '$id'";
+          }
+        return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+    }
+
+    function del($id) {
+        $sql = "DELETE FROM $this->table ";
+        if(is_array($id)) {
+                $tmp = $this->a2s($id);
+                $sql .= " WHERE " . join(" AND ",$tmp);
+            }else{
+                $sql .= " WHERE `id` = '$id'";
+          }
+        return $this->pdo->exec($sql);
+    }
+
+    function save($array) {
+        if(isset($array['id'])) {
+            $sql = "UPDATE $this->table SET ";
+            $tmp = $this->a2s($array);
+            $sql .= join(",",$tmp) . " WHERE `id` = '{$array['id']}'";
+        }else{
+            $sql = "INSERT INTO $this->table ";
+            $key = "`" . join("`,`",array_keys($array)) . "`";
+            $value = "'" . join("','",array_values($array)) . "'";
+            $sql .= "($key) VALUES ($value)";
+        }
+        return $this->pdo->exec($sql);
+    }
+}
+
+$Poster = new DB('poster');
+
+?>
