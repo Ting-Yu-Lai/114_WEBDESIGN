5a0c36d45953ad4b0d568346c6358530a0accf03
Akuma_Yu <manjusaka1110@gmail.com>
2025-07-28 09:16:45 +0800

    完成顯示和刪除功能

--- web03/03_first/back/movie.php
+++ web03/03_first/back/movie.php
@@ -78,14 +78,43 @@
             <button class="sw-btn" onclick="sw(<?= $movie['id'];?>, <?= $prev;?>, 'Movie')">往上</button>
             <button class="sw-btn" onclick="sw(<?= $movie['id'];?>, <?= $next;?>, 'Movie')">往下</button>
             <button>編輯電影</button>
-            <button>刪除電影</button>
+            <button class="del-btn" data-id="<?=$movie['id'];?>">刪除電影</button>
         </div>
         <div>劇情介紹：<?=$movie['intro'];?></div>
     </div>
 </div>
 <?php endforeach;?>
 <script>
+    // 顯示隱藏功能
+    $('.sh-btn').on('click',function(){
+        let id = $(this).data('id');
+        $.post("./api/sh_movie.php",{id},()=>{
+            switch(id) {
+                case "顯示":
+                    $(this).text('隱藏');
+                    break
+                case "隱藏":
+                    $(this).text('顯示');
+                    break
+            }
+            location.reload();
+        })
+    })
+
+    $('.del-btn').on('click',function() {
+        console.log('click ok');
+        
+        let id = $(this).data('id');
+        console.log('id:',id);
+        if(confirm("確定刪除這部電影嗎?")) {
+            $.post("./api/del.php",{table:'Movie',id},()=>{
+                location.reload();
+            })
+        }
+    })
+
     function sw(id, sw, table) {
         $.post('./api/sw.php',{table, id, sw },()=>location.reload());
     }
+
 </script>
