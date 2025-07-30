3b78be5c897e3d031d9f3ffb04e90daae92705ae
Akuma_Yu <manjusaka1110@gmail.com>
2025-07-30 20:22:24 +0800

    完成資料庫class

---
+++ web01/01_five/api/db.php
@@ -0,0 +1,151 @@
+<?php
+session_start();
+date_default_timezone_set("Asia/Taipei");
+
+function to($url)
+{
+    header("Location:$url");
+}
+
+class DB
+{
+    private $table;
+    private $pdo;
+    private $dsn = "mysql:host=localhost;dbname=web01_05;charset=utf8;";
+
+    function __construct($table)
+    {
+        $this->table = $table;
+        $this->pdo = new PDO($this->dsn, 'root', '');
+    }
+
+    function a2s($array)
+    {
+        $tmp = [];
+        foreach ($array as $key => $value) {
+            $tmp[] = "`$key` = '$value'";
+        }
+        return $tmp;
+    }
+
+    function q($sql)
+    {
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    function all(...$arg)
+    {
+        $sql = "SELECT * FROM $this->table ";
+        if (isset($arg[0])) {
+            if (is_array($arg[0])) {
+                $tmp = $this->a2s($arg[0]);
+                $sql .= " WHERE " . join(" AND ", $tmp);
+            } else {
+                $sql .= $arg[0];
+            }
+        }
+        if (isset($arg[1])) {
+            $sql .= $arg[1];
+        }
+        return $this->q($sql);
+    }
+
+    function count(...$arg)
+    {
+        $sql = "SELECT COUNT(*) FROM $this->table ";
+        if (isset($arg[0])) {
+            if (is_array($arg[0])) {
+                $tmp = $this->a2s($arg[0]);
+                $sql .= " WHERE " . join(" AND ", $tmp);
+            } else {
+                $sql .= $arg[0];
+            }
+        }
+        if (isset($arg[1])) {
+            $sql .= $arg[1];
+        }
+        return $this->pdo->query($sql)->fetchColumn();
+    }
+    function sum($col, ...$arg)
+    {
+        $sql = "SELECT SUM($col) FROM $this->table ";
+        if (isset($arg[0])) {
+            if (is_array($arg[0])) {
+                $tmp = $this->a2s($arg[0]);
+                $sql .= " WHERE " . join(" AND ", $tmp);
+            } else {
+                $sql .= $arg[0];
+            }
+        }
+        if (isset($arg[1])) {
+            $sql .= $arg[1];
+        }
+        return $this->pdo->query($sql)->fetchColumn();
+    }
+
+    function max($col, ...$arg)
+    {
+        $sql = "SELECT MAX($col) FROM $this->table ";
+        if (isset($arg[0])) {
+            if (is_array($arg[0])) {
+                $tmp = $this->a2s($arg[0]);
+                $sql .= " WHERE " . join(" AND ", $tmp);
+            } else {
+                $sql .= $arg[0];
+            }
+        }
+        if (isset($arg[1])) {
+            $sql .= $arg[1];
+        }
+        return $this->pdo->query($sql)->fetchColumn();
+    }
+
+    function find($id)
+    {
+        $sql = "SELECT * FROM $this->table ";
+        if (is_array($id)) {
+            $tmp = $this->a2s($id);
+            $sql .= " WHERE " . join(" AND ", $tmp);
+        } else {
+            $sql .= " WHERE `id` = '$id'";
+        }
+        return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+    }
+    
+    function del($id)
+    {
+        $sql = "DELETE FROM $this->table ";
+        if (is_array($id)) {
+            $tmp = $this->a2s($id);
+            $sql .= " WHERE " . join(" AND ", $tmp);
+        } else {
+            $sql .= " WHERE `id` = '$id'";
+        }
+        return $this->pdo->exec($sql);
+    }
+    
+
+    function save($array) {
+        if (isset($array['id'])) {
+            $sql = "UPDATE $this->table SET ";
+            $tmp = $this->a2s($array);
+            $sql .=join(", ", $tmp) . " WHERE `id` = '{$array['id']}'";
+        } else {
+            $sql = "INSERT INTO $this->table ";
+            $keys = "`" . join("`,`",array_keys($array)) . "`";
+            $values = "'" . join("','",array_values($array)) . "'";
+            $sql .= "$keys VALUES $values";
+        }
+        return $this->pdo->exec($sql);
+    }
+
+}
+$Title = new DB ('title');
+$Ad = new DB ('Ad');
+$Mvim = new DB ('mvim');
+$Image = new DB ('image');
+$Footer = new DB ('footer');
+$News = new DB ('news');
+$Admin = new DB ('admin');
+$Menu = new DB ('menu');
+$Total = new DB ('total');
--- web01/01_five/back/add_title.php
+++ web01/01_five/back/add_title.php
@@ -4,14 +4,17 @@
 <hr>
 <form action="./api/add_title.php" method="post" enctype="multipart/form-data">
 
-    <table class="cent">
+    <table class="cent" style="width: 70%;margin:auto;">
         <tr>
-            <td>標題區圖片：</td>
-            <td><input type="file" name="img" id="img"></td>
+            <td style="width: 50%;">標題區圖片：</td>
+            <td style="width: 100%;"><input type="file" name="img" id="img"></td>
         </tr>
         <tr>
-            <td>標題區替代文字：</td>
-            <td><input type="text" name="text" id="text"></td>
+            <td style="width: 50%;" >標題區替代文字：</td>
+            <td style="width: 100%;" ><input type="text" name="text" id="text"></td>
         </tr>
-    </table>
+        </table>
+        <div class="cent">
+            <button type="submit">新增</button><button type="reset">重置</button>
+        </div>
 </form>
