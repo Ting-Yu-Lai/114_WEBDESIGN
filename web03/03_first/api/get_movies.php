4525b739a5340991e788293693b4044ad85a0da2
Akuma_Yu <manjusaka1110@gmail.com>
2025-07-31 09:32:55 +0800

    程式碼-7改成-2

--- web03/03_first/api/get_movies.php
+++ web03/03_first/api/get_movies.php
@@ -3,7 +3,7 @@ include_once 'db.php';
 $id = $_GET['id'];
 
 $today = date("Y-m-d");
-$ondate = date("Y-m-d", strtotime("-7 days",strtotime($today)));
+$ondate = date("Y-m-d", strtotime("-2 days",strtotime($today)));
 $movies = $Movie->all(['sh'=>1]," and ondate between '$ondate' and '$today' order by `rank`");
 
 
