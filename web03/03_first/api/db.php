c8b2ac0dff554d3ec21972084e0100baa9571396
Akuma_Yu <manjusaka1110@gmail.com>
2025-08-01 15:58:24 +0800

    完成訂單管理查看，快速刪除 刪除

--- web03/03_first/api/db.php
+++ web03/03_first/api/db.php
@@ -29,6 +29,13 @@ function to($url)
     header("Location:$url");
 }
 
+function q($sql) {
+    $dsn = "mysql:host=localhost;dbname=web03;charset=utf8;";
+    $pdo = new PDO($dsn, 'root', '');
+    return $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+
+}
+
 
 class DB
 {
