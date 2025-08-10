49b745008697d301cbcce946b0e3645f1b604ffe
Akuma_Yu <manjusaka1110@gmail.com>
2025-08-10 16:00:07 +0800

    完成新增功能

---
+++ web01/01_eight/modal/title.php
@@ -0,0 +1,18 @@
+<div class="cent">
+    <h3>新增網站標題</h3>
+</div>
+<hr>
+<form action="./api/insert.php" method="post" enctype="multipart/form-data">
+    <div class="cent">
+        <label for="img">標題區圖片：</label>
+        <input type="file" name="img" id="">
+    </div>
+    <div class="cent">
+        <label for="img">標題區替代文字：</label>
+        <input type="text" name="text" id="">
+    </div>
+    <div class="cent">
+        <input type="hidden" name="table" value="<?=$_GET['table'];?>">
+        <input type="submit" value="新增"><input type="reset" value="重置">
+    </div>
+</form>
