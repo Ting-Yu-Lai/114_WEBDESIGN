2ad45c89af6b718ed50f3ce6144dff1cdd7d5821
Akuma_Yu <manjusaka1110@gmail.com>
2025-08-11 16:32:39 +0800

    news完成

---
+++ web01/01_eight/modal/news.php
@@ -0,0 +1,14 @@
+<div class="cent">
+    <h3>新增最新消息資料</h3>
+</div>
+<hr>
+<form action="./api/insert.php" method="post" enctype="multipart/form-data">
+    <div class="cent">
+        <label for="img">最新消息資料：</label>
+        <textarea name="text" id="" style="width: 60%;height:100px;"></textarea>
+    </div>
+    <div class="cent">
+        <input type="hidden" name="table" value="<?=$_GET['table'];?>">
+        <input type="submit" value="新增"><input type="reset" value="重置">
+    </div>
+</form>
