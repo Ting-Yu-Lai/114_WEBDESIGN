cc34f10f7495d26cfaff28d1e8bf8ba6f876a058
Akuma_Yu <manjusaka1110@gmail.com>
2025-08-12 13:58:23 +0800

    簡歷檢查帳號

--- web04/04_first/front/login.php
+++ web04/04_first/front/login.php
@@ -1,1 +1,20 @@
-商品
+<h2>第一次購物</h2>
+<a href="?do=reg">
+    <img src="./icon/0413.jpg" alt="">
+</a>
+<h2>會員登入</h2>
+<table class="all">
+    <tr>
+        <td class="tt ct">帳號</td>
+        <td class="pp"><input type="text" name="acc" id="acc"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">密碼</td>
+        <td class="pp"><input type="password" name="pw" id="pw"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">驗證碼</td>
+        <td class="pp"><input type="text" name="chk" id="chk"></td>
+    </tr>
+</table>
+<div class="ct"><button>註冊</button><button>重置</button></div>
---
+++ web04/04_first/front/reg.php
@@ -0,0 +1,44 @@
+<h2>會員註冊</h2>
+<table class="all">
+    <tr>
+        <td class="tt ct">姓名</td>
+        <td class="pp"><input type="text" name="name" id="name"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">帳號</td>
+        <td class="pp">
+            <input type="text" name="acc" id="acc">
+            <button onclick="chkAcc()">檢測帳號</button>
+        </td>
+    </tr>
+    <tr>
+        <td class="tt ct">密碼</td>
+        <td class="pp"><input type="password" name="pw" id="pw"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">電話</td>
+        <td class="pp"><input type="text" name="tel" id="tel"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">住址</td>
+        <td class="pp"><input type="text" name="addr" id="addr"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">電子郵件</td>
+        <td class="pp"><input type="email" name="email" id="email"></td>
+    </tr>
+</table>
+<div class="ct"><button>註冊</button><button>重置</button></div>
+<script>
+    function chkAcc() {
+        let acc = $("#acc").val;
+        $.get("./api/chkAcc.php", {acc}, (res)=> {
+            if(parseInt(res)>0 || acc == 'admin') {
+                alert("帳號已存在");
+            }else{
+                alert("帳號可用")
+            }
+        })
+    }
+    
+</script>
