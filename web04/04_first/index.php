2e1f95e979dbac5c1d1a435a3c253c2fc25b1564
Akuma_Yu <manjusaka1110@gmail.com>
2025-08-12 11:12:34 +0800

    完成最新消息與購物流程

--- web04/04_first/index.php
+++ web04/04_first/index.php
@@ -12,21 +12,23 @@
 </head>
 
 <body>
-    <iframe name="back" style="display:none;"></iframe>
+
     <div id="main">
         <div id="top">
-            <a href="?">
+            <a href="index.php">
                 <img src="./icon/0416.jpg">
             </a>
             <div style="padding:10px;">
-                <a href="?">回首頁</a> |
+                <a href="index.php">回首頁</a> |
                 <a href="?do=news">最新消息</a> |
                 <a href="?do=look">購物流程</a> |
                 <a href="?do=buycart">購物車</a> |
                 <a href="?do=login">會員登入</a> |
                 <a href="?do=admin">管理登入</a>
             </div>
-            情人節特惠活動 &nbsp; 為了慶祝七夕情人節，將舉辦情人兩人到現場有七七折之特惠活動~
+            <marquee behavior="" direction="">
+                情人節特惠活動~ &nbsp;&nbsp;&nbsp; 情人節特惠活動~ &nbsp;&nbsp;&nbsp;
+            </marquee>
         </div>
         <div id="left" class="ct">
             <div style="min-height:400px;">
