dc8107331eddf52339da9939296738bf3e34ee32
Akuma_Yu <manjusaka1110@gmail.com>
2025-08-18 10:41:50 +0800

    完成大中分類撈出資料

--- web04/04_first/back/th.php
+++ web04/04_first/back/th.php
@@ -8,18 +8,38 @@
     新增中分類
     <select name="selbig" id="selbig"></select>
     <input type="text" name="mid" id="mid">
-    <button>新增</button>
+    <button onclick="addMid()">新增</button>
 </div>
 
 <table class="all">
+    <?php 
+    $bigs = $Type->all(['big_id'=>0]);
+    foreach($bigs as $big):
+    ?>
     <tr class="tt">
-        <td>假資料</td>
-        <td class="ct"><button>修改</button><button>刪除</button></td>
+        <td><?=$big['name'];?></td>
+        <td class="ct">
+            <button class="edit-btn" data-id="<?=$big['id'];?>">修改</button>
+            <button class="edit-btn" data-id="<?=$big['id'];?>">刪除</button>
+        </td>
     </tr>
+    <?php
+    if($Type->count(['big_id'=>$big['id']])>0):
+    $mids = $Type->all(['big_id'=>$big['id']]);
+    foreach($mids as $mid):
+    ?>
     <tr class="ct pp">
-        <td>123</td>
-        <td class="ct"><button>修改</button><button>刪除</button></td>
+        <td><?=$mid['name'];?></td>
+        <td class="ct">
+            <button class="edit-btn" data-id="<?=$mid['id'];?>">修改</button>
+            <button class="del-btn" data-id="<?=$mid['id'];?>">刪除</button>
+        </td>
     </tr>
+    <?php
+    endforeach;
+    endif;
+    endforeach;
+    ?>
 </table>
 
 <h2 class="ct">商品管理</h2>
@@ -48,10 +68,34 @@
 </table>
 
 <script>
+    getBigs();
     function addBig() {
         let name=$("#big").val();
         $.post("./api/save_type.php",{name,big_id:0},()=>{
-
+            $("#big").val();
+            getBigs();
         })
     }
+    
+    function addMid() {
+        let name=$("#mid").val();
+        let big_id=$("#selBig").val();
+        $.post("./api/save_type.php",{name,big_id},()=>{
+            location.reload();
+        })
+    }
+    function getBigs() {
+        $.get("./api/get_bigs.php",(options)=>{
+            $("#selBig").html(options);
+        })
+    }
+
+    $(".del-btn").on("click",function(){
+        let id = $(this).data('id');
+        if(confirm("確定要刪除這筆分類資料嗎?")) {
+            $.post("./api/del.php",{id,table:Type},()=>{
+                location.reload();
+            })
+        }
+    })
 </script>
