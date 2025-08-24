97b49d98fc20a58c5b5ad9de9cc6855b129f64ed
Akuma_Yu <manjusaka1110@gmail.com>
2025-08-24 17:35:42 +0800

    完成問卷調查 投票與結果顯示

---
+++ practice/web02/02_third/front/news.php
@@ -0,0 +1,92 @@
+<style>
+    .title {
+        cursor: pointer;
+        color: blue;
+    }
+
+    .title:hover {
+        text-decoration: underline;
+        color: green;
+    }
+
+    .pop {
+        background: rgba(51, 51, 51, 0.8);
+        color: #FFF;
+        min-height: 100px;
+        width: 500px;
+        position: fixed;
+        display: none;
+        z-index: 9999;
+        overflow: auto;
+    }
+</style>
+
+<fieldset>
+    <legend>目前位置:首頁 > 最新    文章區</legend>
+    <table>
+        <tr class="ct">
+            <td width="20%">標題</td>
+            <td width="60%">內容</td>
+        </tr>
+        <?php
+        $all = count($News->all());
+        $div = 5;
+        $page = ceil($all / $div);
+        $now = $_GET['p'] ?? 1;
+        $start = ($now - 1) * $div;
+        $rows = $News->all(" order by `good` desc limit $start,$div");
+        foreach ($rows as $id => $row):
+        ?>
+            <tr class="ct">
+                <td class="title"><?= $row['title']; ?></td>
+                <td >
+                    <div class="short">
+                        <?= mb_substr($row['text'], 0, 30); ?>...
+                    </div>
+                    <div class="all" style="display: none;"><?=nl2br($row['text']);?></div>
+                    </div>
+                </td>
+                <td>
+                    <?php
+                    if(isset($_SESSION['login'])):
+                        $chk = $Log->count(['news'=>$row['id'],'user'=>$_SESSION['login']]);
+                    ?>
+                    <a href="#" onclick="good(<?=$row['id'];?>)"><?=($chk)?'-收回讚':'-讚';?></a>
+                    <?php endif;?> 
+                </td>
+            </tr>
+        <?php
+        endforeach;
+        ?>
+    </table>
+    <div>
+        <?php
+        if ($now - 1 > 0):
+        ?>
+            <a href="?do=news&p=<?= $now - 1; ?>">
+                < </a>
+                <?php endif; ?>
+                <?php
+                for ($i = 1; $i <= $page; $i++):
+                    $size = ($now == $i) ? "24px" : '';
+                ?>
+                    <a href="?do=news&p=<?= $i; ?>" style="font-size: <?= $size; ?>"><?= $i; ?></a>
+                <?php endfor; ?>
+                <?php
+                if ($now + 1 <= $page):
+                ?>
+                    <a href="?do=news&p=<?= $now + 1; ?>"> > </a>
+                <?php endif; ?>
+    </div>
+</fieldset>
+<script>
+    $('.title').on("click",function(){
+        $(this).next().find(".short,.all").toggle();
+    })
+    
+    function good(news) {
+        $.post("./api/good.php",{news},function(){
+            location.reload();
+        })
+    }
+</script>
